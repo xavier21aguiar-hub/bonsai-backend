56a03a407c9757a49ea58b8a09dbc9f40f8714b2
xaguiar265 <x.aguiar@cimmyt.org>
2026-05-07 11:08:04 -0600

    Coneccion

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-    await mongoose.connect("mongodb+srv://admin:ThroughAdmin245@cluster0.jqjhgap.mongodb.net/?appName=Cluster0");
+    await mongoose.connect("process.env.MONGO_URI");
     console.log("🟢 MongoDB conectado");
     } catch (error) {
     console.error("🔴 Error MongoDB:", error);
