eced68e330dc46e525303d2f4fd698f1eeb68694
xaguiar265 <x.aguiar@cimmyt.org>
2026-05-07 11:57:33 -0600

    Fixed moongose

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,15 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        console.log("MONGO URI:", process.env.MONGO_URI);
-        await mongoose.connect("process.env.MONGO_URI");
+        
+        const mongoURI = process.env.MONGO_URI;
+
+        console.log("MONGO URI:", mongoURI);
+
+        await mongoose.connect(mongoURI);
+
         console.log("🟢 MongoDB conectado");
+
     } catch (error) {
         console.error("🔴 Error MongoDB:", error);
     }
